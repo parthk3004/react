d99c7c1b54795ef6ea049d454582743241fa9cc2
Parth Korawala <50219075+parthk3004@users.noreply.github.com>
2020-09-11 09:40:35 +0530

    Add files via upload

---
+++ assignment 2/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Stylesheet from "./components/Stylesheet.js";
+import "fontsource-roboto";
+
+function App() {
+  return (
+    <div className="App">
+      <Stylesheet />
+    </div>
+  );
+}
+
+export default App;
