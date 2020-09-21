74b20173a1a146cf189f7e0b1bc95fb043c2a83b
Parth Korawala <50219075+parthk3004@users.noreply.github.com>
2020-09-21 10:59:08 +0530

    Create header.js

---
+++ weather app/header.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h1> <font  color: red;
+    font-size: 2rem;
+    margin-left: 20px;
+    vertical-align: middle;>Weather Application <font></h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
