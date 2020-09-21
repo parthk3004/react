8311dd9e9656f3fe7de810eddb44eae5565187db
Parth Korawala <50219075+parthk3004@users.noreply.github.com>
2020-09-21 09:56:13 +0530

    Create logger.js

---
+++ calc/src/components/logger.js
@@ -0,0 +1,21 @@
+const {
+    createLogger,
+    format,
+    transports,
+} = require("winston");
+
+const logger = createLogger({
+    transports: [
+        new transports.File({
+            filename: 'error.log',
+            level: 'error',
+
+        }),
+        new transports.File({
+            filename: 'info.log',
+            level: 'info',
+            format: format.combine(format.timestamp(), format.json())
+        })
+    ]
+})
+module.exports = logger;
